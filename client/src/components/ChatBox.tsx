dad02ab81c8f78d14027258f069eb6bf7dc01767
Aljith KJ <kwdeathlok@gmail.com>
2024-01-01 15:33:15 +0530

    implement websocket client

--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux"
 import { StoreType } from "../store"
 import { getMessages } from "../apis/chat"
 import { IMessage } from "../interfaces/chat.interface"
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
 const style1 = 'self-start bg-[#202c33] px-3 py-2 rounded-lg max-w-[50%] rounded-tl-none relative'
 const style2 = 'self-end bg-[#005c4b] px-3 py-2 rounded-lg max-w-[50%]  rounded-tr-none relative'
@@ -10,9 +11,36 @@ const style2 = 'self-end bg-[#005c4b] px-3 py-2 rounded-lg max-w-[50%]  rounded-
 export const ChatBox = () => {
   const [messages, setMessages] = useState<IMessage[]>([]);
   const { roomId, myId } = useSelector((data: StoreType) => data.chats);
+  const [client, setClient] = useState<W3CWebSocket | null>(null);
   
   useEffect(() => {
+    const token = localStorage.getItem('token') || 'token';
+    
     roomId && fetchMessages();
+
+    const client = new W3CWebSocket(`ws://localhost:3000/${token}`, 'echo-protocol');
+    setClient(client);
+
+    client.onerror = function(err) {
+        console.log('Connection Error', err.message);
+    };
+    
+    client.onopen = function() {
+        console.log('WebSocket Client Connected');
+    };
+
+    client.onclose = function() {
+        console.log('echo-protocol Client Closed');
+    };
+    
+    client.onmessage = function(e) {
+      if(typeof e.data === 'string') {
+        const newMessage: IMessage = JSON.parse(e.data);
+        
+        setMessages((msg) => [newMessage, ...msg])
+      }
+    };
+
   }, [roomId])
 
   const fetchMessages = async () => {
@@ -22,6 +50,17 @@ export const ChatBox = () => {
     if(data) {
       setMessages(data.reverse());
     }
+  }
+
+  const sendMessage = () => {
+    if(!client) return;
+    client.send(JSON.stringify({
+      type: "SEND_MESSAGE",
+      payload: {
+        roomId,
+        message: "Good Nyt"
+      }
+    }))
   }
 
   return (
@@ -37,6 +76,7 @@ export const ChatBox = () => {
           </div>
         )
       })}
+      <button className="bg-red-600" onClick={sendMessage}>TEST ME</button>
     </div>
   )
 }
